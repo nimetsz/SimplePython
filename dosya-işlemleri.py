55fb29351ed10705db49afa6c41610a891b9c37d
nimetsz <94058174+nimetsz@users.noreply.github.com>
2021-11-15 20:52:11 +0300

    dosyaişlemi

---
+++ dosya-işlemleri.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 15 00:59:48 2020
+
+@author: nsazo
+"""
+
+f=open("bnmDosya.txt", "w")
+f.write("merhaba\n")
+f.write("nasılsın?\n")
+f.write("iyi misin?")
+f.close()
